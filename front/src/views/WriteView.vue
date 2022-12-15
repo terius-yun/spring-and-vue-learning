d8b92678902d2e06cd4964f2c9d1662cc9f857f7
terius <tingtangting9398@gmail.com>
2022-12-15 17:12:26 +0900

    Spring learning - CORS config 생성
    Vue3 learning
    - vue project create
    - bootstrap, element-plus install
    - axios를 이용한 기본적인 통신

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+import axios from "axios"
+
+const  title = ref("")
+const  content = ref("")
+
+const write = function (){
+  axios.post("http://localhost:8080/posts",{
+    title : title.value,
+    content : content.value
+  });
+}
+</script>
+
+<template>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해주세요."/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15" />
+  </div>
+
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글 작성 저장</el-button>
+  </div>
+</template>
+
+<style>
+
+</style>
